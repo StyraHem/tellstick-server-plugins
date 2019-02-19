94d04fb76c745fd19eec3b383469765a0ed821a5
Micke Prag <micke.prag@telldus.se>
2019-02-19 15:41:50 +0100

    Pylint fixes

--- shelly/setup.py
+++ shelly/setup.py
@@ -2,22 +2,21 @@
 # -*- coding: utf-8 -*-
 
 try:
-  from setuptools import setup
-  from setuptools.command.install import install
+	from setuptools import setup
+	from setuptools.command.install import install
 except ImportError:
-  from distutils.core import setup
-  from distutils.command.install import install
+	from distutils.core import setup
+	from distutils.command.install import install
 import os
 
-class buildweb(install):
+class BuildwebInstall(install):
 	def run(self):
 		print("generate web application")
 		if os.system('npm install') != 0:
 			raise Exception("Could not install npm packages")
 		if os.system('npm run build') != 0:
 			raise Exception("Could not build web application")
-		bdist_egg.run(self)
-	    #install.run(self)
+		install.run(self)
 
 setup(
 	name='Shelly',
@@ -30,12 +29,14 @@ setup(
 	description='Shelly smart home devices',
 	long_description='',
 	packages=['shelly'],
-	package_dir = {'':'src'},
-  	cmdclass={'install': buildweb},  #Call the fuction buildweb
+	package_dir={'':'src'},
+	cmdclass={  # Build web interface
+		'install': BuildwebInstall,
+	},
 	entry_points={ \
 		'telldus.startup': ['c = shelly:Shelly [cREQ]']
 	},
-	extras_require = dict(cREQ = 'Base>=0.1\nTelldus>=0.1\nTelldusWeb>=0.1'),
+	extras_require=dict(cREQ='Base>=0.1\nTelldus>=0.1\nTelldusWeb>=0.1'),
 	package_data={'shelly' : [
 			'htdocs/img/*.jpg',
 			'htdocs/style/*.css',
